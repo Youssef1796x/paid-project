b36c026c3f8a9075d56736d3c51d6514e5aca0fb
Youssef1796x <joo2007pro@gmail.com>
2026-09-09 21:46:59 +0300

    Use file-based Open Graph metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,39 +8,9 @@ const cairo = Cairo({
   subsets: ["arabic"],
 });
 
-const siteUrl = process.env.VERCEL_PROJECT_PRODUCTION_URL
-  ? `https://${process.env.VERCEL_PROJECT_PRODUCTION_URL}`
-  : process.env.VERCEL_URL
-    ? `https://${process.env.VERCEL_URL}`
-    : "http://localhost:3000";
-
-const shareImageUrl = new URL(restaurantConfig.banner, siteUrl).toString();
-
 export const metadata: Metadata = {
-  metadataBase: new URL(siteUrl),
   title: restaurantConfig.name,
   description: restaurantConfig.description,
-  openGraph: {
-    title: restaurantConfig.name,
-    description: restaurantConfig.description,
-    url: siteUrl,
-    type: "website",
-    locale: "ar_EG",
-    images: [
-      {
-        url: shareImageUrl,
-        width: 960,
-        height: 400,
-        alt: restaurantConfig.name,
-      },
-    ],
-  },
-  twitter: {
-    card: "summary_large_image",
-    title: restaurantConfig.name,
-    description: restaurantConfig.description,
-    images: [shareImageUrl],
-  },
 };
 
 export default function RootLayout({
