b9219864c1317eb3899d699174f2cc64ad526573
Youssef1796x <joo2007pro@gmail.com>
2026-09-09 21:49:27 +0300

    Fix Open Graph build error from Arabic font shaping

--- app/opengraph-image.tsx
+++ app/opengraph-image.tsx
@@ -1,7 +1,6 @@
 import { ImageResponse } from "next/og";
-import { restaurantConfig } from "@/src/data/restaurant";
 
-export const alt = `${restaurantConfig.name} - ${restaurantConfig.tagline}`;
+export const alt = "K & Runch";
 export const size = {
   width: 1200,
   height: 630,
@@ -29,32 +28,21 @@ export default function OpenGraphImage() {
         <div
           style={{
             display: "flex",
-            fontSize: 30,
+            fontSize: 32,
             color: "#F59E0B",
             marginBottom: 24,
           }}
         >
-          {restaurantConfig.tagline}
+          K & Runch
         </div>
         <div
           style={{
             display: "flex",
             fontSize: 76,
             fontWeight: 800,
-            marginBottom: 24,
           }}
         >
-          {restaurantConfig.name}
-        </div>
-        <div
-          style={{
-            display: "flex",
-            fontSize: 32,
-            color: "#D4D4D4",
-            maxWidth: 900,
-          }}
-        >
-          {restaurantConfig.description}
+          Egyptian Restaurant
         </div>
       </div>
     ),
