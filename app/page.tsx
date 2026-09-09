c1d28df42f56ae6e11894d9299dcdc79cbe095f3
Youssef1796x <joo2007pro@gmail.com>
2026-09-09 22:01:29 +0300

    Restore Reviews section on homepage

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import Navbar from "@/src/components/Navbar";
 import Hero from "@/src/components/Hero";
 import Menu from "@/src/components/Menu";
 import About from "@/src/components/About";
+import Reviews from "@/src/components/Reviews";
 import Location from "@/src/components/Location";
 import Footer from "@/src/components/Footer";
 
@@ -13,6 +14,7 @@ export default function Home() {
         <Hero />
         <Menu />
         <About />
+        <Reviews />
         <Location />
       </main>
       <Footer />
