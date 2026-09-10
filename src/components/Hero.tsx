ad5ee166cb4014ec01ec5244658587a9e1defa48
Yossef1796x <joo2007pro@gmail.com>
2026-09-10 21:42:49 +0300

    new

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -30,7 +30,7 @@ export default function Hero() {
           </div>
         </div>
 
-        <div className="order-1 mx-auto w-full max-w-105 overflow-hidden rounded-[28px] border-3 border-[#e8623a45] lg:order-2">
+        <div className="order-1 mx-auto w-full max-w-140 overflow-hidden rounded-[28px] border-3 border-[#e8623a45] lg:order-2">
           <Image
             src={restaurantConfig.banner}
             alt={`بانر مطعم ${restaurantConfig.name}`}
